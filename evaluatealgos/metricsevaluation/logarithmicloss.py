602e4002a6825c6238d5ed377989b034c97d8df1
phuong <phuongphamdps@gmail.com>
2020-01-15 12:05:22 +0700

    Machine Learning

---
+++ evaluatealgos/metricsevaluation/logarithmicloss.py
@@ -0,0 +1,23 @@
+# Cross Validation Classification Accuracy
+from pandas import read_csv
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+
+# load data
+filename = '../../data/pima-indians-diabetes.csv'
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+
+dataframe = read_csv(filename, names=names)
+
+array = dataframe.values
+X = array[:,0:8]
+Y = array[:,8]
+
+
+kfold = KFold(n_splits=10, random_state=7)
+model = LogisticRegression()
+scoring = 'neg_log_loss'
+results = cross_val_score(model, X, Y, cv=kfold, scoring=scoring)
+
+print('Accuracy: {:.3}  {:.3}'.format(results.mean(), results.std()))
